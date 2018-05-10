31937bb7f0faecedfad142966a8cf867267d3e95
John Horton <john.joseph.horton@gmail.com>
2018-05-10 13:43:51 -0400

    Selects lowest vote-getter at random + simulates running

--- stv_results.py
+++ stv_results.py
@@ -3,6 +3,11 @@
 import csv
 import re
 import copy 
+import random
+
+import os
+import sys
+from contextlib import contextmanager
 
 def OrderChoices(ordering, names):
     "Creates a list for each respondent, with choices ordered 1 = most-preferred"
@@ -23,17 +28,18 @@ def SimulateChoiceData(num_options, num_voters):
         preferences.append(combo)
     return preferences 
 
-def ActualChoiceData(file_name):
+def ActualChoiceData(file_name, file_includes_timestamps = False):
     "Returns a list of all preferences as well as the names of all the choices."
     # reads in the data from a CSV file. Structure is timestamp, then the rank ordering e.g., 
     # ['2018/05/09 10:55:53 AM AST', '7', '2', '1', '9', '4', '3', '5', '6', '8']
+    start_index = 1 if file_includes_timestamps else 0 
     preferences = []; # will store everyone's preferences 
     with open(file_name, 'rb') as csvfile:
         csvreader = csv.reader(csvfile)
         header = csvreader.next()
-        original_names = [re.search("\[.*", h).group(0) for h in header[1::]]  # grabs the choice names from the header row (starts at 1 b/c of timestamp)
+        original_names = [re.search("\[.*", h).group(0) for h in header[start_index::]]  # grabs the choice names from the header row (starts at 1 b/c of timestamp)
         for index, row in enumerate(csvreader):
-            preferences.append(OrderChoices(tuple(row[1::]), original_names))
+            preferences.append(OrderChoices(tuple(row[start_index::]), original_names))
     return {'preferences':preferences, 'original_names':original_names}
 
 def GetVotingOutcome(preferences):
@@ -62,10 +68,11 @@ def WorstPerformingName(voting_outcomes, names):
         else:
             d[name] = voting_outcomes[name]
     fewest_votes = min(d.values())
+    names_with_fewest_votes = []
     for name in d.keys():
         if d[name] == fewest_votes:
-            break
-    return name
+            names_with_fewest_votes.append(name)
+    return random.choice(names_with_fewest_votes)
 
 def BestPerformingName(voting_outcomes):
     "Gets the best performing name"
@@ -128,22 +135,42 @@ def FindWinner(preferences, original_names, cutoff):
         print ""
         print "Fraction of vote going to top-voting name %s" % frac_going_to_top
         if frac_going_to_top > cutoff:
+            winning_name = BestPerformingName(voting_outcomes)
             print "Habemus Papam! Our new name is: %s" % BestPerformingName(voting_outcomes)
             break 
         else:
             worst_name = WorstPerformingName(voting_outcomes, names)
-            print "We need top vote-getter to have %s of the vote." % CUTOFF
+            print "We need top vote-getter to have %s of the vote." % cutoff
             print "We're dropping from consideration: %s" % worst_name
             new_preferences = PurgePreferences(new_preferences, worst_name)
             names.remove(worst_name)
+    return winning_name
 
 # Actual results 
-voting_results = ActualChoiceData("faculty_vote_results.csv")
+voting_results = ActualChoiceData("faculty_vote_results_no_timestamp.csv")
 FindWinner(voting_results['preferences'], voting_results['original_names'], 0.5)
 
 # Simulated results 
-num_choices = 5
-num_voters = 30
-preferences = SimulateChoiceData(num_choices, num_voters)
-choice_names = range(num_choices)
-FindWinner(preferences, choice_names, 0.5)
+# num_choices = 5
+# num_voters = 30
+# preferences = SimulateChoiceData(num_choices, num_voters)
+# choice_names = range(num_choices)
+# FindWinner(preferences, choice_names, 0.5)
+
+
+# Used for running many times w/o dumping print statements to stdout
+@contextmanager
+def silence_stdout():
+    new_target = open(os.devnull, "w")
+    old_target, sys.stdout = sys.stdout, new_target
+    try:
+        yield new_target
+    finally:
+        sys.stdout = old_target
+
+# Simulate running num_simulations times 
+num_simulations = 100
+with silence_stdout():
+    winners = [FindWinner(voting_results['preferences'], voting_results['original_names'], 0.5) for i in range(num_simulations)]
+
+print(set(winners))
